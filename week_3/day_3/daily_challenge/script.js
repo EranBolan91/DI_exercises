44ab58dc48dd4e38942c072b69591fda37980c04
eran <eranbolan91@gmail.com>
2020-01-28 19:24:47 +0200

    Daily challenge - not completed yet!

---
+++ week_3/day_3/daily_challenge/script.js
@@ -0,0 +1,37 @@
+let sentences = [
+    "What came first,the chichken or the NOUN .",
+    "Be careful not to vacuum the NOUN when you clean under your bed.",
+    "BBQ at my house! Everyones invited, and its bring your own NOUN ",
+    "One that ADJECTIVE music comes on,its time to shut down your acceptance speech!",
+    "Dear NAME you are NOUN ",
+    "Please excuse NAME who is far too ADJECTIVE to attend NOUN class",
+    "I cant start my day without NOUN"
+]
+
+let nounInput = document.getElementById("noun");
+let adjectiveInput = document.getElementById("adjective");
+let personInput = document.getElementById("person");
+
+let count = 0;
+var libButton = document.getElementById('lib-button');
+    var libIt = function() {
+
+        let sente = sentences[count];
+        var storyDiv = document.getElementById("story");
+
+        if(adjectiveInput.value != ""){
+            storyDiv.innerHTML = sente.replace("ADJECTIVE",adjectiveInput.value); 
+            console.log(adjectiveInput.value);   
+        }
+        if(nounInput.value != ""){
+            storyDiv.innerHTML = sente.replace("NOUN",nounInput.value);
+            console.log(nounInput.value);
+            
+        }
+        if(personInput.value != ""){
+            storyDiv.innerHTML = sente.replace("NAME",personInput.value);
+            console.log(personInput.value);
+        }
+        count++;
+    };
+    libButton.addEventListener('click', libIt);
